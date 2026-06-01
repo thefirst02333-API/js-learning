0d295147640e9f718f4676550c89a26b83469412
Meet <thefirst02333@gmail.com>
2026-06-01 21:32:05 +0530

    Day 1 -  arrow functions and typeof

---
+++ Day1.js
@@ -0,0 +1,5 @@
+const greet = name =>{
+    return "Hello, I am " + name;
+}
+
+console.log(greet("Meet"))
---
+++ index.js
@@ -0,0 +1,5 @@
+const greet = (name)=>{
+    return "Hello, this is " + name + ". I am going to ship my first API"
+}
+
+console.log(greet("Meet")); 
