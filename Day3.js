600dd07394f485858a8b5633d9f2246bc5ce2258
Meet <thefirst02333@gmail.com>
2026-06-04 09:16:34 +0530

    Day 3 - arrays and push

---
+++ Day3.js
@@ -0,0 +1,9 @@
+const holidays = ["Republic Day", "Holi", "Diwali", "Christmas"];
+
+console.log(holidays);
+console.log(holidays[0]);
+console.log(holidays[2]);
+
+holidays.push("Eid");
+console.log(holidays);
+console.log(holidays.length);
